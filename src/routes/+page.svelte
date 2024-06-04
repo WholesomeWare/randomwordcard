7fb2f8c88cf2a5ab741cc4f8c0e7d4f89e08b3a6
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-06-05 00:01:21 +0200

    New main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,83 @@
+<script>
+    import Fab from "$lib/components/Fab.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import Panel from "$lib/components/Panel.svelte";
+    import { mdiRefresh } from "@mdi/js";
+    import { text } from "@sveltejs/kit";
+
+    const words = [
+        "dobókocka",
+        "társasjáték",
+        "lottó",
+        "szerencse",
+        "véletlen",
+        "kockáztatás",
+        "esély",
+        "valószínűség",
+        "fogadás",
+        "rulet",
+        "kaszinó",
+        "jóslat",
+        "megérzés",
+        "improvizáció",
+        "kaland",
+        "felfedezés",
+        "meglepetés",
+        "izgalom",
+        "feszültség",
+        "kihívás",
+        "szerencsejáték",
+        "káoosz",
+        "rendetlenség",
+        "zűrzavar",
+        "váratlan fordulat",
+        "balszerencse",
+        "sors",
+        "karma",
+        "hiba",
+        "tévedés",
+        "katasztrófa",
+        "végzet",
+        "csoda",
+        "rejtély",
+        "paradoxon",
+        "dilemma",
+    ];
+
+    let randomWord = $state(words[Math.floor(Math.random() * words.length)]);
+</script>
+
 <main>
     <h1>Random Szókártya</h1>
 
     <p>Érintsd a telefonod egy kártyához a kezdéshez.</p>
+
+    <p>Nincs kártyád? Itt egy random szó:</p>
+
+    <Panel>
+        <p style="text-align: center;">{randomWord}</p>
+    </Panel>
+
+    <Fab
+        iconPath={mdiRefresh}
+        text="Új szó"
+        onclick={() =>
+            (randomWord = words[Math.floor(Math.random() * words.length)])}
+    />
+
+    <p>
+        A projekt jelenleg béta fázisban van, ezért még nincsenek eladó kártyák.
+        Kövesd a WholesomeWare Insta oldalát, hogy értesülj az újdonságokról!
+    </p>
 </main>
+
+<Footer />
 
 <style>
     main {
         display: flex;
         flex-direction: column;
-        gap: 1rem;
+        gap: 2rem;
         padding: 1rem;
     }
 </style>
